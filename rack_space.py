413f4a6cb37afc82db76163d0085a5f2c2caf3d5
Andy Situ <and.situ@gmail.com>
2017-04-14 16:40:34 -0700

    slight changes in values

--- rack_space.py
+++ rack_space.py
@@ -75,7 +75,7 @@ def get_racks_y(y):
 		if put_lane:
 			if lane_left_1 < (l_y - MARGIN_LANE_SPACE):
 				rack_1 -= 1
-				lane_left_1 += r_y * 2
+				lane_left_1 += r_y
 				break
 			lane_left_1 -= l_y
 			empty_lane_1 += 1
@@ -88,6 +88,7 @@ def get_racks_y(y):
 			rack_1 += 2
 			# print(lane_left_1, "Racks: ", rack_1, "EMPTY LANES ", empty_lane_1)
 			put_lane = True
+		# print(empty_lane_1, rack_1, rack_1*r_y + empty_lane_1*l_y)
 	# if rack_1 / 2 - 1 != empty_lane_1:
 	# 	rack_1 -= 2
 
@@ -234,7 +235,7 @@ def get_dimensions(percentage, total_sq_ft):
 
 def get_max_min_x(total_sq_ft):
 	min_x_percentage = 0.2
-	max_x_percentage = 0.75
+	max_x_percentage = 0.80
 	#Returns the max and min dimensions by x ranging from 20% to 50%
 	min_x = get_dimensions(min_x_percentage, total_sq_ft)["x"]
 	max_x = get_dimensions(max_x_percentage, total_sq_ft)["x"]
@@ -249,37 +250,57 @@ def get_racks(total_sq_ft):
 	max_x = math.ceil(max_min_x["max_x"])
 	min_x =int(max_min_x["min_x"])
 
+	# x & y are the varying dimensinos of the simulated warehouse in ft
 	for x in range(min_x, max_x+1):
+		y = total_sq_ft / x
 		rack_dict = {}
 
 		# Testing if rounding x up or down will give sq ft closer
 		# to total sq ft
-		y1 = int(total_sq_ft / x)
-		y2 = math.ceil(total_sq_ft / x)
-		dif1 = total_sq_ft - y1 * x
-		dif2 = total_sq_ft - y2 * x
-		if dif1 > dif2:
-			y = y2
-		else:
-			y = y1
+		# y1 = int(total_sq_ft / x)
+		# y2 = math.ceil(total_sq_ft / x)
+		# dif1 = total_sq_ft - y1 * x
+		# dif2 = total_sq_ft - y2 * x
+		# if dif1 > dif2:
+		# 	y = y2
+		# else:
+		# 	y = y1
 
 		racks = get_best_racks(x, y)
-		rack_dict["y_racks_dict"] = copy.deepcopy(racks["y_racks_dict"])
-		rack_dict["x_racks_dict"] = copy.deepcopy(racks["x_racks_dict"])
-		rack_dict["total_racks_ground"] = rack_dict["y_racks_dict"]["rack"] * rack_dict["x_racks_dict"]["rack"]
+		y_racks_dict = copy.deepcopy(racks["y_racks_dict"])
+		x_racks_dict = copy.deepcopy(racks["x_racks_dict"])
+		# rack_dict["y_racks_dict"] = y_racks_dict
+		# rack_dict["x_racks_dict"] = x_racks_dict
+		rack_dict["num of racks"] = x_racks_dict["rack"]
+		rack_dict["num of racks aisles"] = y_racks_dict["rack"]
+		rack_dict["warehouse width_ft"] = x
+		rack_dict["warehouse length_ft"] = y
+		rack_dict["total_racks_ground"] = y_racks_dict["rack"] * x_racks_dict["rack"]
 		rack_dict["total_racks"] = rack_dict["total_racks_ground"] * level_racks
 		rack_dict["total_pallets"] = 2 * rack_dict["total_racks"]
 		rack_dict["total_pallet_sq_ft"] = rack_dict["total_pallets"] * PALLET_SPACE
 		rack_dict["total_sq_ft"] = total_sq_ft
-		rack_dict["min_warehouse_total_cost"] = total_sq_ft * MIN_SQ_FT_COST
-		rack_dict["max_warehouse_total_cost"] = total_sq_ft * MAX_SQ_FT_COST
-		rack_dict["max_sq_ft_charge"] = (rack_dict["max_warehouse_total_cost"] * (1 + PROFIT_MARGIN)) / rack_dict["total_pallet_sq_ft"]
-		rack_dict["min_sq_ft_charge"] = (rack_dict["min_warehouse_total_cost"] * (1 + PROFIT_MARGIN)) / rack_dict["total_pallet_sq_ft"]
 
+		min_warehouse_cost = total_sq_ft * MIN_SQ_FT_COST
+		max_warehouse_cost = total_sq_ft * MAX_SQ_FT_COST
+		rack_dict["min_warehouse_total_cost"] = min_warehouse_cost
+		rack_dict["max_warehouse_total_cost"] = max_warehouse_cost
+
+		min_profit_total = (min_warehouse_cost * (1 + PROFIT_MARGIN))
+		max_profit_total = (max_warehouse_cost * (1 + PROFIT_MARGIN))
+
+		rack_dict["min_sq_ft_charge"] = min_profit_total / rack_dict["total_pallet_sq_ft"]
+		rack_dict["max_sq_ft_charge"] = max_profit_total / rack_dict["total_pallet_sq_ft"]
+		rack_dict["min cost per pallet"] = min_profit_total / rack_dict["total_pallets"]
+		rack_dict["max cost per pallet"] = max_profit_total / rack_dict["total_pallets"]
+		pallet_cubic_meter = rack_dict["total_racks"] * 40 * 48 * 72 / 1728 / 35.3147
+		rack_dict["total pallet cubic meter"] = pallet_cubic_meter
+		rack_dict["min cost by cubic meter"] = min_profit_total / pallet_cubic_meter
+		rack_dict["max cost by cubic meter"] = max_profit_total / pallet_cubic_meter
 		racks_dict[x] = rack_dict
 
-		print(rack_dict)
-	# print(racks_dict)
+		# print(rack_dict)
+	return racks_dict
 
 def calculate_warehouse_space(rack_aisles, rack_rows, empty_lane_aisles):
 	rack_space = rack_aisles * rack_rows * RACK_SPACE
@@ -288,3 +309,42 @@ def calculate_warehouse_space(rack_aisles, rack_rows, empty_lane_aisles):
 
 	total_sq_ft = rack_space + lanes_space + side_lanes_space * 2
 	return total_sq_ft
+
+import openpyxl	
+
+alphabet_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'L', 'K', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+def print_racks_to_excel(racks_dict, name):
+	wb = openpyxl.Workbook()
+	sheet = wb.active
+	key_added = False
+	rack_key = None
+	num_racks = len(racks_dict)
+	keys_list  = []
+
+	for k, rack_dict in racks_dict.items():
+		for rack_k, rack_v in rack_dict.items():
+			keys_list.append(rack_k)
+		break
+
+	for i, key in enumerate(keys_list):
+		sheet[alphabet_list[i] + "1"] = key
+
+	for num_rack, width in enumerate(racks_dict):
+		rack_dict = racks_dict[width]
+		for i, key in enumerate(keys_list):
+			sheet[alphabet_list[i] + str(num_rack + 2)] = rack_dict[key]
+
+	# for k, rack_dict in racks_dict.items():
+	# 	for rack_k, rack_v in rack_dict.items():
+	# 		if not key_added:
+	# 			rack_key = rack_dict.keys()
+	# 			key_added = True
+	# 			rack_keys_len = len(rack_key)
+	# 			for i in range(0, rack_keys_len):
+	# 				letter = alphabet_list[i]
+	# 				sheet[letter + '1'] = rack_k
+		# rack_key = rack_dict.keys()
+		# print(rack_key)
+
+	wb.save(str(name) + ".xlsx")
